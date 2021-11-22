3990bb4087a06f3aa410c0572df28e545982238a
Ivan Bautrukevich <ibautrukevich@exadel.com>
2021-11-22 15:26:20 +0300

    STEP-4 init SpringBoot with disabled liquibase and SpringData

---
+++ src/main/java/com/instructure/shop/TheCourseShopApplication.java
@@ -0,0 +1,13 @@
+package com.instructure.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCourseShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCourseShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instructure/shop/TheCourseShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instructure.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCourseShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
